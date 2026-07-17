8a79ceeb7b7cf02131daf41f00098226370efcd0
suchi9900 <sojibvondro@gmail.com>
2026-07-17 16:08:43 +0600

    Update fork.js

--- Script/commands/fork.js
+++ Script/commands/fork.js
@@ -2,8 +2,8 @@ module.exports.config = {
     name: "fork",
     version: "1.0.1",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
-    description: "Send YouTube channel and GitHub fork link with intro text",
+    credits: "SAJIB Fucker",
+    description: "Not",
     commandCategory: "other",
     usages: "fork",
     cooldowns: 0,
@@ -11,12 +11,9 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event }) {
     const message = 
-        "🌟 আসসালামু আলাইকুম সম্মানিত বট ইউজার 🌟\n\n" +
-        "আপনি যদি তৈরি করতে চান, তাহলে চলে যেতে পারেন আমাদের ইউটিউব চ্যানেলে। " +
-        "সেখানে গিয়ে ভিডিও দেখে আপনি খুব সহজে বট তৈরি করে নিতে পারবেন। " +
-        "তাহলে আর দেরি না করে এখনি চলে যান আমাদের ইউটিউব চ্যানেলে।\n\n" +
-        "➤ YouTube Channel: https://youtube.com/@scb-community\n\n" +
-        "🔗 GitHub Fork Link: https://github.com/shahadat-sahu/SHAHADAT-CHAT-BOT";
-
+        "ওয়েল কাম বোকাচোদা রা, সজীবের পক্ষ থেকে স্বাগতম সবাইকে!
+    
+        "🔗 GitHub Fork Link: সজীবের ইনবক্সে..!😒";
+     "https://i.imgur.com/ZhfQKNU.jpeg"
     return api.sendMessage(message, event.threadID, event.messageID);
 };
